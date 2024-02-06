8c61e49d1f650464f03062e39a571c2ef7ffdce4
unknown <aleksandarstanojevic93@gmail.com>
2024-02-07 00:23:30 +0100

    Removed strict

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ import Header from './components/Header.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+const isLogged = false;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
-          <Route path="Shop" element={<Shop />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="Shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
