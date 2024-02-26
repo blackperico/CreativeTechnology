0c823412abd22e249cce83f17d1be47c2dcf4afb
unknown <aleksandarstanojevic93@gmail.com>
2024-02-26 22:23:43 +0100

    Function checking if email is present in accounts JSON and Function displaying response message

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,16 +1,46 @@
-import '../styles/ResetPassword.css';
+import '../styles/AccountOperations.css';
+import accountsResponse from '../accounts.json';
+import { useState } from 'react';
 
 function ResetPassword() {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState(null);
+
+    function renderErrorMessage() {
+        if(error === null)
+            return null;
+        if(error === true)
+            return <p className='error-message'>Email not found.</p>;
+        if(error === false)
+            return <p className='error-message' style={{color: 'green'}}>New password sent to email.</p>;
+    };
+
+    function isPresent() {
+        let notFound = true;
+        
+        for(let account of accountsResponse) {
+            if(account.email === email) {
+                notFound = false;
+                break;
+            }
+        }
+        
+        return notFound;
+    };
+
     function handleSubmit(e) {
         e.preventDefault();
+        
+        setError(isPresent());
+    };
 
-    }
     return(
         <div id="container">
             <h3>Reset password</h3>
             <form onSubmit={handleSubmit}>
                 <label htmlFor='email'>Enter your email: </label>
-                <input id='email' className='input-box' type='text'></input>
+                <input id='email' className='input-box' type='text' onChange={(e) => setEmail(e.target.value)}></input>
+                    {renderErrorMessage()}
                 <button id='reset-button' type='submit'>Reset password</button>
             </form>
         </div>
