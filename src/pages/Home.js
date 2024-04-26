48bc4074debb858dbadc1f00811b20ac7dad1c1e
unknown <aleksandarstanojevic93@gmail.com>
2024-04-26 21:03:53 +0200

    Holds array of objects each representing funkyBox, passing it down to FunkyBox

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import '../styles/Home.css';
-import { useEffect, useRef, useState } from 'react';
-import userEvent from '@testing-library/user-event';
 import Articles from '../components/Articles';
 import FunkyBox from '../components/FunkyBox';
 import MainRight from '../components/MainRight';
+import featured from '../funkybox/featured.json';
+import topsellers from '../funkybox/topsellers.json';
+import recommended from '../funkybox/recommended.json';
 
 function Main() {
     const funkyBoxes = [
@@ -11,19 +12,28 @@ function Main() {
             title: 'Top-sellers',
             mainColor: 'rgba(0, 255, 255, 1)',
             hoverColor: 'rgba(0, 255, 255, 0.6)',
-            dropdownColor: 'rgba(0, 255, 255, 0.2)'
+            dropdownColor: 'rgba(0, 255, 255, 0.2)',
+            overscrollColor: '',
+            scrollIndicatorColor: '',
+            json: topsellers
         },
         {
             title: 'Featured',
             mainColor: 'rgba(255, 0, 255, 1)',
             hoverColor: 'rgba(255, 0, 255, 0.6)',
-            dropdownColor: 'rgba(255, 0, 255, 0.2)'
+            dropdownColor: 'rgba(255, 0, 255, 0.2)',
+            overscrollColor: '',
+            scrollIndicatorColor: '',
+            json: featured
         },
         {
             title: 'Recommended',
             mainColor: 'rgba(255, 255, 0, 1)',
             hoverColor: 'rgba(255, 255, 0, 0.6)',
-            dropdownColor: 'rgba(255, 255, 0, 0.2)'
+            dropdownColor: 'rgba(255, 255, 0, 0.2)',
+            overscrollColor: '',
+            scrollIndicatorColor: '',
+            json: recommended
         }
     ];
 
