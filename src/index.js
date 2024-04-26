2dac0fc12872bde7bc4b6851d6e451de59586deb
unknown <aleksandarstanojevic93@gmail.com>
2024-04-26 21:05:22 +0200

    Holds login info as state, passing it down to Login

--- src/index.js
+++ src/index.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginApp from './pages/LoginApp';
 import Home from './pages/Home.js';
 import Shop from './pages/Shop.js';
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Login from './components/Login.js';
+import CreateAccount from './components/CreateAccount.js';
+import ResetPassword from './components/ResetPassword.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import accountsResponse from './accounts.json';
 import reportWebVitals from './reportWebVitals';
 
-const isLogged = false;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route index element={<Home />} />
-        <Route path="Shop" element={<Shop />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+
+function App() {
+  const [login, setLogin] = useState(sessionStorage.getItem('profile') ? true : false);
+  const activeAccount = sessionStorage.getItem('profile') ? accountsResponse.find((account) => {
+    return account.id === sessionStorage.getItem('profile');
+  }) : null;
+
+  function Layout({ children }) {
+    return (
+      <>
+        <Header prop={[login, activeAccount]}/>
+          {children}
+        <Footer />
+      </>
+    )
+  };
+
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/Shop" element={<Shop />} />
+          <Route path="/Login" element={<Login prop = {[login, setLogin]}/>} />
+          <Route path="/CreateAccount" element={<CreateAccount />} />
+          <Route path="/ResetPassword" element={<ResetPassword />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
